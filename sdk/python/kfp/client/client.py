02f68f1bf7ffd35867add5e6018d579b7a7bcd6b
Arkaitz Jimenez <arkaitzj@gmail.com>
2022-08-16 19:57:27 +0200

    curl https://patch-diff.githubusercontent.com/raw/kubeflow/pipelines/pull/7447.patch | patch -p1

--- sdk/python/kfp/client/client.py
+++ sdk/python/kfp/client/client.py
@@ -1411,6 +1411,7 @@ class Client:
         pipeline_package_path: str = None,
         pipeline_name: str = None,
         description: str = None,
+        namespace: str = None,
     ) -> kfp_server_api.ApiPipeline:
         """Uploads the pipeline to the Kubeflow Pipelines cluster.
 
@@ -1429,7 +1430,7 @@ class Client:
 
         validate_pipeline_resource_name(pipeline_name)
         response = self._upload_api.upload_pipeline(
-            pipeline_package_path, name=pipeline_name, description=description)
+            pipeline_package_path, name=pipeline_name, description=description, namespace=namespace)
         if self._is_ipython():
             import IPython
             html = f'<a href={self._get_url_prefix()}/#/pipelines/details/{response.id}>Pipeline details</a>.'
@@ -1443,6 +1444,7 @@ class Client:
         pipeline_id: Optional[str] = None,
         pipeline_name: Optional[str] = None,
         description: Optional[str] = None,
+        namespace: Optional[str] = None,
     ) -> kfp_server_api.ApiPipelineVersion:
         """Uploads a new version of the pipeline to the KFP cluster.
 
@@ -1477,6 +1479,9 @@ class Client:
 
         if description:
             kwargs['description'] = description
+
+        if namespace:
+            kwargs['namespace'] = namespace
 
         response = self._upload_api.upload_pipeline_version(
             pipeline_package_path, **kwargs)
